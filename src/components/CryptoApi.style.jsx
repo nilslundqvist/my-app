ba59267393d8b6b5458903890c589ac7c23a30fe
nilslundqvist <nils.g.lundqvist@hotmail.com>
2021-12-28 01:57:46 +0100

    api skit

---
+++ src/components/CryptoApi.style.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const CryptoApi = styled.div`
+    width: fit-content;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    background: linear-gradient(to right bottom, rgba(255,255,255,0.7),rgba(255,255,255,0.3));
+    
+    height: 5rem;
+    z-index: 3;
+    
+    border-radius: 15px;
+    
+`
